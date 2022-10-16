9f79f45a725a62a5ee060f0924b8d2dcde2800b2
Mary Elizabeth <maryelizabeth@Marys-MacBook-Pro.local>
2022-10-16 11:52:46 -0500

    first commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopolo
     () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
 )
 
-// MIDDLEWARE
+// MIDDLEWARE/
 app.use(express.urlencoded({extended: true}))
 
 // ROUTES
